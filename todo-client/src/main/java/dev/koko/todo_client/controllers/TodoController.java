8626fa5b15acd3b4941895d36fe69abd0eda7e60
ko-kym <koutaroukym000@gmail.com>
2025-03-06 13:37:17 +0000

    feat: リクエスト確認

--- todo-client/src/main/java/dev/koko/todo_client/controllers/TodoController.java
+++ todo-client/src/main/java/dev/koko/todo_client/controllers/TodoController.java
@@ -1,13 +1,28 @@
 package dev.koko.todo_client.controllers;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TodoController {
     @GetMapping("")
-    public String index() {
-        
+    public String index() throws URISyntaxException, IOException, InterruptedException {
+        String url = "http://localhost:8080/api/v1/todos";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI(url))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         return "index";
     }
 }
